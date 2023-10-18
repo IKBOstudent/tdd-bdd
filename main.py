c9259cbb9217a958bb6afa3beaadc37f79974203
IKBOstudent <90282668+IKBOstudent@users.noreply.github.com>
2023-10-18 23:35:50 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+def main():
+    while True:
+        print("Выберите опцию калькулятора:")
+        print("1. Квадратное уравнение")
+        print("2. Сумма матриц")
+        print("3. Транспонирование матриц")
+        print("4. Получить обратную матрцу")
+        print("5. Посчитать факториал")
+        print("0. Выход")
+
+        choice = input("Введите номер опции: ")
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            pass
+        elif choice == "0":
+            break
+        else:
+            print("Неверный выбор. Пожалуйста, выберите снова.")
+
+
+if __name__ == "__main__":
+    main()
