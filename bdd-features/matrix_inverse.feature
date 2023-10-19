05ffff1807e5ef5ce867fc26785d6918424e8a02
MarkTerris <64842407+MarkTerris@users.noreply.github.com>
2023-10-19 11:49:13 +0000

    feat: added matrix inverse behave steps

--- bdd-features/matrix_inverse.feature
+++ bdd-features/matrix_inverse.feature
@@ -1,10 +1,10 @@
 Feature: Inverse Matrix
   Scenario: Inverse valid matrix
-    Given [[2, 1], [4, 3]]
+    Given matrix [[2, 1], [4, 3]]
     When Inversing
     Then result should be [[1.5, -0.5], [-2, 1]]
 
   Scenario: Inverse singular matix
-    Given [[1, 2], [2, 4]]
+    Given matrix [[1, 2], [2, 4]]
     When Inversing
     Then result should be None
--- bdd-features/steps/matrix_sum_test.py
+++ bdd-features/steps/matrix_sum_test.py
@@ -1,21 +1,19 @@
 from behave import given, when, then
-from main import matrix_sum
+from main import get_inverse_matrix
 import numpy as np
 
 
-@given("{matrix1} and {matrix2}")
-def step_given_two_matrix(context, matrix1, matrix2):
-    context.matrix1 = eval(matrix1)
-    context.matrix2 = eval(matrix2)
+@given("matrix {matrix}")
+def step_given_matrix(context, matrix):
+    context.matrix = eval(matrix)
 
 
-@when("summing")
+@when("inversing")
 def step_summing_matrix(context):
-    context.result = matrix_sum(context.matrix1, context.matrix2)
-    print(context.matrix1, context.matrix2, context.result)
+    context.result = get_inverse_matrix(context.matrix)
 
 
-@then("expect result to be {expected_result}")
+@then("result should be {expected_result}")
 def step_then_verify_result(context, expected_result):
     assert np.array_equal(eval(expected_result), context.result)
 
